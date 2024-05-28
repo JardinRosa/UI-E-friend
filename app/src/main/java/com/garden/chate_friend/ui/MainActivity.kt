147db4e3b1f7911a16117034abca060816c22b76
Polar1920 <dmpp1920@gmail.com>
2024-05-27 21:23:58 -0500

    xd

--- app/src/main/java/com/garden/chate_friend/ui/MainActivity.kt
+++ app/src/main/java/com/garden/chate_friend/ui/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.garden.chate_friend.ui
 
+import MessagingAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.garden.chate_friend.R
+import com.garden.chate_friend.data.Message
+import com.garden.chate_friend.utils.Constants.SEND_ID
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var messagingAdapter: MessagingAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        messagingAdapter = MessagingAdapter()
+
+        val rvMessages = findViewById<RecyclerView>(R.id.rv_messages)
+        rvMessages.adapter = messagingAdapter
+        rvMessages.layoutManager = LinearLayoutManager(this) // Asignar un LinearLayoutManager al RecyclerView
+
+        val btnEnviar = findViewById<Button>(R.id.btn_voice)
+        btnEnviar.setOnClickListener {
+            print("Se envio un mensaje")
+            val etMessage = findViewById<EditText>(R.id.et_message)
+            val messageText = etMessage.text.toString()
+
+            val message = Message(messageText, SEND_ID, "00:00")
+
+            messagingAdapter.insertMessage(message)
+
+            etMessage.text.clear()
+            print("Fin")
+
+            messagingAdapter.simulateAutoReply()
+        }
     }
 }
--- app/src/main/java/com/garden/chate_friend/ui/MessagingAdapter.kt
+++ app/src/main/java/com/garden/chate_friend/ui/MessagingAdapter.kt
@@ -1,5 +1,3 @@
-package com.garden.chate_friend.ui
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +7,19 @@ import com.garden.chate_friend.R
 import com.garden.chate_friend.data.Message
 import com.garden.chate_friend.utils.Constants.RECEIVE_ID
 import com.garden.chate_friend.utils.Constants.SEND_ID
-import javax.net.ssl.SSLEngine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MessagingAdapter: RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
+class MessagingAdapter : RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
 
     var messagesList = mutableListOf<Message>()
+
     inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        init{
-            itemView.setOnClickListener{
+        init {
+            itemView.setOnClickListener {
                 messagesList.removeAt(adapterPosition)
                 notifyItemRemoved(adapterPosition)
             }
@@ -24,17 +27,43 @@ class MessagingAdapter: RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        return MessageViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false))
+        return MessageViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
         return messagesList.size
     }
 
-    fun insertMessage(message: Message){
-        this.messagesList.add(message)
-        notifyItemInserted(messagesList.size)
+    fun insertMessage(message: Message) {
+        GlobalScope.launch(Dispatchers.Main) { // Cambio importante aquí
+            messagesList.add(message)
+            notifyItemInserted(messagesList.size)
+            if (message.id == SEND_ID) {
+                simulateAutoReply()
+            }
+        }
     }
+
+
+    fun simulateAutoReply() {
+        val autoReplyMessage = Message("Respuesta automática", RECEIVE_ID, getCurrentTime())
+        GlobalScope.launch {
+            kotlinx.coroutines.delay(2000)
+            insertMessage(autoReplyMessage)
+        }
+    }
+
+    fun getCurrentTime(): String {
+        val dateFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
+        return dateFormat.format(Date())
+    }
+/*
+    fun sendMessageToAPI(message: String) {
+
+    } */
+
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val currentMessage = messagesList[position]
